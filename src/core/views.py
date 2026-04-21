62824678a0da89454b70e2b3b2a834331b9e6422
Esteban <esteban.horacio.acevedo.gmail.com>
2026-04-20 22:21:00 -0300

    Crea pruebas en core

--- src/core/views.py
+++ src/core/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+
+def home(request):
+    return render(request, "core/index.html")
+
+
+def saludar(request):
+    return HttpResponse("Hola!!!")
+
+
+def saludar_con_etiqueta(request):
+    return HttpResponse("<h1>Hola!!!</h1><p>cómo estás</p>")
